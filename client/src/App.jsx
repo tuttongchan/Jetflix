4f88b783ab7b0ebe0ae38a3b4ac3124f8b747555
Tut Tongchantra <tuttongchan@Tuts-MacBook-Air.local>
2021-09-04 04:54:05 -0700

    added Admin files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,47 @@
-import "./app.scss"
-import Home from "./pages/home/Home";
-import Register from "./pages/register/Register";
-import Watch from "./pages/watch/Watch";
-import Login from "./pages/login/Login";
+import './app.scss';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import Home from './pages/home/Home';
+import Register from './pages/register/Register';
+import Watch from './pages/watch/Watch';
+import Login from './pages/login/Login';
 
 const App = () => {
-  return <Home/>;
+  // const user = false;
+  const user = true;
+
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {user ? <Home /> : <Redirect to="/register" />}
+        </Route>
+        <Route exact path="/register">
+          {!user ? <Register /> : <Redirect to="/" />}
+        </Route>
+        <Route exact path="/login">
+          {!user ? <Login /> : <Redirect to="/" />}
+        </Route>
+        {user && (
+          <>
+            <Route path="/movies">
+              <Home type="movies" />
+            </Route>
+            <Route path="/series">
+              <Home type="series" />
+            </Route>
+            <Route path="/watch">
+              <Watch type="watch" />
+            </Route>
+          </>
+        )}
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,17 +1,44 @@
-import Navbar from "../../components/navbar/Navbar";
-import Featured from "../../components/featured/Featured";
-import "./home.scss";
-import List from "../../components/list/List";
+import { useEffect, useState } from 'react';
+import './home.scss';
+import Navbar from '../../components/navbar/Navbar';
+import Featured from '../../components/featured/Featured';
+import List from '../../components/list/List';
+import axios from 'axios';
 
-const Home = () => {
+const Home = ({ type }) => {
+  const [lists, setLists] = useState([]);
+  const [genre, setGenre] = useState(null);
+
+  useEffect(() => {
+    const getRandomLists = async () => {
+      try {
+        const res = await axios.get(
+          `lists${type ? '?type=' + type : ''}&${
+            genre ? '&genre=' + genre : ''
+          }`,
+          {
+            headers: {
+              token:
+                'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMzE4MWFlNzk0MDZlNmQ1YzUwZmM1MyIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2MzA3NDg2NjEsImV4cCI6MTYzMTE4MDY2MX0.Ug0V59oZMMgMT_PLvDnQrW1BwiiwCUI8U987MGkg5sU',
+            },
+          }
+        );
+        console.log(res.data);
+        setLists(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getRandomLists();
+  }, [genre, type]);
+
   return (
     <div className="home">
       <Navbar />
-      <Featured/>
-      <List/>
-      <List/>
-      <List/>
-      <List/>
+      <Featured type={type} />
+      {lists.map((list) => (
+        <List list={list} />
+      ))}
     </div>
   );
 };
